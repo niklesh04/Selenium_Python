71c1727cabcd3571f5a56de8cf869591edbe0824
Niklesh <kondaniklesh65@gmail.com>
2020-04-06 22:50:46 +0530

    first commit

---
+++ POM_1/Test/login_test.py
@@ -0,0 +1,45 @@
+import HtmlTestRunner
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+from POM_1.Pages.login_page import login_page
+from POM_1.Pages.home_page import home_page
+
+class LoginTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+        cls.driver.implicitly_wait(5)
+        cls.driver.maximize_window()
+
+    def test_login_valid(self):
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+
+        login = login_page(driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+
+        homepage = home_page(driver)
+        homepage.click_welcome()
+        homepage.click_logout()
+
+        # self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        # self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        # self.driver.find_element_by_id("btnLogin").submit()
+        # self.driver.find_element_by_id("welcome").click()
+        # self.driver.find_element_by_link_text("Logout").click()
+
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(5)
+        cls.driver.quit()
+        print("Test Competed")
+
+    if __name__ == '__main__':
+        unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output="C:/Users/Niklesh/PycharmProjects/Selenium/reports"))
